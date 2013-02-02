ae3b408418c4bf5036c9db9272a8bc55f2256b45
Sean <seanp2k@gmail.com>
2013-02-01 22:37:36 -0800

    Update gsd.sh
    Updated default case, added documentation, fixed quoting.

--- gsd.sh
+++ gsd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-case $1 in
+case "$1" in
 
 "add")
-  grep $2 /etc/hosts || echo " 127.0.0.1 $2" >> /etc/hosts
+  grep "$2" /etc/hosts || echo " 127.0.0.1 $2" >> /etc/hosts
 ;;
 "remove")
-  grep $2 /etc/hosts && sed -i "/$2/d" /etc/hosts
+  grep "$2" /etc/hosts && sed -i "/$2/d" /etc/hosts
 ;;
 "on")
   sed -i "s/#/ /g" /etc/hosts
@@ -14,4 +14,6 @@ case $1 in
 "off")
   sed -i "s/^ /#/" /etc/hosts
 ;;
+*)
+  echo 'Usage: $0 ( (add | remove) some.fqdn | (on | off) ) 
 esac
